e2fe32c509a47fe99aea94c7f5611daa2d1518bd
Phu Loc Nguyen <phuloc01.nguyen@gmail.com>
2024-10-09 10:06:52 +0700

    update

---
+++ src/base/llm_model.py
@@ -0,0 +1,30 @@
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, BitsAndBytesConfig
+
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True, 
+    bnb_4bit_quant_type="nf4", 
+    bnb_4bit_use_double_quant=True, 
+    bnb_4bit_compute_dtype=torch.bfloat16)
+
+def get_LLM(model_name: str = "mistralai/Mistral -7B-Instruct -v0.2",
+            max_new_token=1024,
+            **kwargs):
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, 
+        quantization_config=nf4_config,
+        low_cpu_mem_usage=True
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    model_pipeline = pipeline(
+        "text_generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_token = max_new_token,
+        pad_token_id=tokenizer.eos_token_id
+    )
+    llm = HuggingFacePipeline(pipeline=model_pipeline, model_kwargs=kwargs)
+    return llm
